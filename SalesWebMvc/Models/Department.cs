36c172557be5e8e1cfca9a5c135a0750efddabbc
Anderson de Melo Damaceno <dinho.live01@gmail.com>
2021-08-14 23:38:12 -0300

    SeedingService

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,13 +1,14 @@
-﻿using System.Collections.Generic;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+
 namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public ICollection<Seller> Seller { get; set; } = new List<Seller>();
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
         public Department()
         {
@@ -21,13 +22,12 @@ namespace SalesWebMvc.Models
 
         public void AddSeller(Seller seller)
         {
-            Seller.Add(seller);
+            Sellers.Add(seller);
         }
 
         public double TotalSales(DateTime initial, DateTime final)
         {
-            return Seller.Sum(seller => seller.TotalSales(initial, final));
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
         }
     }
 }
-
